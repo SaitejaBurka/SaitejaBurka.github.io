2f5b65958ede7f460d0c532c769bf981640955ac
SaitejaBurka <saitejaburka2@gmail.com>
2023-02-13 17:55:24 +0530

    files added

---
+++ project.js
@@ -0,0 +1,36 @@
+//E-commerce project
+const product1 = {
+    id: 1,
+    name: ''Tayto'',
+    description: 'Cheese & Onion Chips',
+    img: 'https://www.irishtimes.com/polopoly_fs/1.4078148!/image/image.jpg'
+    createdAt: '2020-09-20'
+};
+
+const product2 = {
+    id: 2,
+    name: ''Water'',
+    description: 'Mineral water',
+    img: 'http://www.mazalv.com/wp-content/uploads/2016/11/Bottled-Water1-979x1024-1-979x1024.png'
+    createdAt: '2020-09-20'
+};
+
+
+//Social Media project
+const post1 = {
+    id: 1,
+    name: 'My first post',
+    text: 'This is my first post',
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Post-greenland-uummannaq.jpg/1200px-Post-greenland-uummannaq.jpg"
+    author: 'Andres Lowles',
+    createdAt: '2020-09-20'
+};
+
+const post2 = {
+    id: 2,
+    name: 'My second post',
+    text: 'This is my second post',
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Post-greenland-uummannaq.jpg/1200px-Post-greenland-uummannaq.jpg"
+    author: 'Andres Lowles',
+    createdAt: '2020-09-20'
+};
